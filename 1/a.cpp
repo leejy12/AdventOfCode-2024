b39e055b807dc041df60464669d0ca9795c6e481
Junyoung Lee <leejy31415@outlook.com>
2024-12-01 17:16:00 +0900

    clang-format

--- 1/a.cpp
+++ 1/a.cpp
@@ -6,20 +6,19 @@
 #include <vector>
 
 int main() {
-  int l, r;
-  std::vector<int> left, right;
+    int l, r;
+    std::vector<int> left, right;
 
-  while (std::cin >> l >> r) {
-    left.push_back(l);
-    right.push_back(r);
-  }
+    while (std::cin >> l >> r) {
+        left.push_back(l);
+        right.push_back(r);
+    }
 
-  std::ranges::sort(left);
-  std::ranges::sort(right);
+    std::ranges::sort(left);
+    std::ranges::sort(right);
 
-  const auto diffs = std::views::zip_transform(
-      [](int x, int y) { return std::abs(x - y); }, left, right);
-  const auto answer = std::accumulate(diffs.begin(), diffs.end(), 0);
+    const auto diffs = std::views::zip_transform([](int x, int y) { return std::abs(x - y); }, left, right);
+    const auto answer = std::accumulate(diffs.begin(), diffs.end(), 0);
 
-  std::println("{}", answer);
+    std::println("{}", answer);
 }
--- 1/b.cpp
+++ 1/b.cpp
@@ -4,21 +4,21 @@
 #include <vector>
 
 int main() {
-  int l, r;
-  std::vector<int> left;
-  std::map<int, int> cnt;
+    int l, r;
+    std::vector<int> left;
+    std::map<int, int> cnt;
 
-  while (std::cin >> l >> r) {
-    left.push_back(l);
-    cnt[r]++;
-  }
-
-  int score = 0;
-  for (const int x : left) {
-    if (cnt.contains(x)) {
-      score += x * cnt[x];
+    while (std::cin >> l >> r) {
+        left.push_back(l);
+        cnt[r]++;
     }
-  }
 
-  std::println("{}", score);
+    int score = 0;
+    for (const int x : left) {
+        if (cnt.contains(x)) {
+            score += x * cnt[x];
+        }
+    }
+
+    std::println("{}", score);
 }
