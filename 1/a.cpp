ef55fde0daa85a37fc6741e85b2c3d9cd3dea759
Junyoung Lee <leejy31415@outlook.com>
2024-12-01 17:12:54 +0900

    clang-format

--- 1/a.cpp
+++ 1/a.cpp
@@ -1,27 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <ranges>
-#include <print>
 #include <algorithm>
+#include <iostream>
 #include <numeric>
+#include <print>
+#include <ranges>
+#include <vector>
 
-int main()
-{
-    int l, r;
-    std::vector<int> left, right;
+int main() {
+  int l, r;
+  std::vector<int> left, right;
 
-    while (std::cin >> l >> r)
-    {
-        left.push_back(l);
-        right.push_back(r);
-    }
+  while (std::cin >> l >> r) {
+    left.push_back(l);
+    right.push_back(r);
+  }
 
-    std::ranges::sort(left);
-    std::ranges::sort(right);
+  std::ranges::sort(left);
+  std::ranges::sort(right);
 
-    const auto diffs = std::views::zip_transform([](int x, int y) { return std::abs(x - y); }, left, right);
-    const auto answer = std::accumulate(diffs.begin(), diffs.end(), 0);
+  const auto diffs = std::views::zip_transform(
+      [](int x, int y) { return std::abs(x - y); }, left, right);
+  const auto answer = std::accumulate(diffs.begin(), diffs.end(), 0);
 
-    std::println("{}", answer);
+  std::println("{}", answer);
 }
-
