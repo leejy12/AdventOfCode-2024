995f72580fd003182c15b5e26ead02296941b2c1
Junyoung Lee <leejy31415@outlook.com>
2024-12-01 17:12:43 +0900

    Day 1 - Part 2

---
+++ 1/b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+#include <print>
+#include <vector>
+
+int main() {
+  int l, r;
+  std::vector<int> left;
+  std::map<int, int> cnt;
+
+  while (std::cin >> l >> r) {
+    left.push_back(l);
+    cnt[r]++;
+  }
+
+  int score = 0;
+  for (const int x : left) {
+    if (cnt.contains(x)) {
+      score += x * cnt[x];
+    }
+  }
+
+  std::println("{}", score);
+}
